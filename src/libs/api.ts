1cc438d9c9307e851a018fb20296b43671f7d84e
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-10-24 16:47:29 +0700

    Refactoring: Add url base cho domain

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,1 +1,9 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+
+export const MOVIE_BASE_URL = API_BASE_URL + '/movies';
+export const GENRE_BASE_URL = API_BASE_URL + '/genres';
+export const COUNTRY_BASE_URL = API_BASE_URL + '/countries';
+export const SERVER_BASE_URL = API_BASE_URL + '/servers';
+export const VERSION_BASE_URL = API_BASE_URL + '/versions';
+export const AUTH_BASE_URL = API_BASE_URL + '/auth';
+export const STATS_BASE_URL = API_BASE_URL + '/stats';
